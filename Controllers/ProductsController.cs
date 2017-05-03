8deb8960cd1043b4de8590cf9b8e055f0bd315d3
Krzysztof Goljasz <arturg007@gmail.com>
2017-05-03 22:01:00 +0200

    Show list products from database in view

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProductsAspNetCore.Models;
+using ProductsAspNetCore.Data;
 
 namespace ProductsAspNetCore.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ProductContext context;
+
+        public ProductsController(ProductContext _context)
+        {
+            context=_context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(context.Products.ToList());
         }
 
     }
