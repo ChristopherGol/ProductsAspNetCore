dc9708c690cf2e8406960f1f410a0631ee029903
Krzysztof Goljasz <arturg007@gmail.com>
2017-05-03 22:16:23 +0200

    Added annotations to product model(display name)

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,14 +1,26 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductsAspNetCore.Models
 {
     public class Product 
     {
         public int ID { get; set; }
+
+        [DisplayName("Nazwa")]
         public string Name { get; set; }
+
+        [DisplayName("Kod kreskowy EAN")]
         public string EAN { get; set; }
+
+        [DisplayName("Cena jednostkowa")]
         public double Price { get; set; }
+
+        [DisplayName("Jednostka")]
         public string Unit { get; set; }
+
+        [DisplayName("Stan magazynowy")]
         public int Availability { get; set; }
     }
 }
